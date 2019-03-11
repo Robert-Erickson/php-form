87d366671df3604ef251aab448591f05a84ab113
Robert Michael Erickson <robert.erickson@uvm.edu>
2019-03-11 17:22:11 -0400

    6: PHP Security check (testing - set to fail on purpose)

---
+++ lib/security.php
@@ -0,0 +1,30 @@
+<?php
+print PHP_EOL . '<!--  BEGIN include security -->' . PHP_EOL;
+$token = 'replace with token from course module notes';
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// performs a simple security check to see if our page has submitted the form to itself
+function securityCheck($myFormURL = "") {
+    $debugThis = false;  // you have to specifically want to test this
+    
+    $status = false; // start off thinking everything is good until a test fails
+    
+    // when it is a form page check to make sure it submitted to itself
+    if ($myFormURL != "") {
+        $fromPage = htmlentities($_SERVER['HTTP_REFERER'], ENT_QUOTES, 'UTF-8');
+        
+        //remove http or https
+        $fromPage = preg_replace('#^https?:#', '', $fromPage);
+        
+        if ($debugThis)
+            print '<p>From: ' . $fromPage . ' should match your Url: ' . $myFormURL;
+        
+        if ($fromPage == $myFormURL) {
+            $status = true;
+        }
+    }
+    return false; // returning false on purpose to see it fail    
+    return $status;
+}
+print PHP_EOL . '<!--  END include security NOTE: ' . $token . ' -->' . PHP_EOL;
+?>
