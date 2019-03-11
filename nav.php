d1d84c8d0dc3603a15af173b7d47bed74979c42b
Robert Michael Erickson <robert.erickson@uvm.edu>
2019-03-11 17:01:42 -0400

    1: Initial template setup for my form project

---
+++ nav.php
@@ -0,0 +1,28 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav>
+    <ol>
+        <?php
+        // NOTE: you already have a nav and just need to add form
+
+	// Repeat this if block for each menu item 
+        // designed to give the current page a class but also allows
+        // you to have more classes if you need them
+        print '<li class="';
+        if ($path_parts['filename'] == "index") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="index.php">Home</a>';
+        print '</li>';
+
+        /* example of repeating */
+        print '<li class="';
+        if ($path_parts['filename'] == "form") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<a href="form.php">Join</a>';
+        print '</li>';
+        ?>
+    </ol>
+</nav>
